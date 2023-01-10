3968207e12b58c760ff0700b69bbc262e174cb1c
Ju <108021184+eujuliabrendha@users.noreply.github.com>
2023-01-10 10:39:29 -0300

    Add files via upload

---
+++ Desafio 01.py
@@ -0,0 +1,7 @@
+('Crie um script ython que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.')
+
+print ('====Desafio 01====')
+
+nome=input ('Qual é seu nome?')
+
+print ('Oi' + nome + ', prazer em te conhecer!')
