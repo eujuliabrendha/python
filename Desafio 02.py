c98a7a4193b1738b0579cc6cb1bc322822b4c938
Ju <108021184+eujuliabrendha@users.noreply.github.com>
2023-01-10 11:01:28 -0300

    Add files via upload

---
+++ Desafio 02.py
@@ -0,0 +1,12 @@
+#Crie um script Python que leia o dia,
+#o mês e o ano de nascimento de uma pessoa
+#e mostre uma mensagem com a data formatada.
+
+
+print ('>>> Desafio 02 <<<')
+
+dia= input ('Qual o dia que você nasceu? ')
+mês= input ('Em que mês você nasceu? ')
+ano= input ('Você é de que ano? ')
+
+print ('Você nasceu no dia', dia, 'de', mês, 'de', ano, ', certo?')
