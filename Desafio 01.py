4e23be60a18b003b207ed38b06bbc5f66bd2e1e6
Ju <108021184+eujuliabrendha@users.noreply.github.com>
2023-01-10 10:40:01 -0300

    Update Desafio 01.py

--- Desafio 01.py
+++ Desafio 01.py
@@ -1,4 +1,4 @@
-('Crie um script ython que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.')
+('Crie um script python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.')
 
 print ('====Desafio 01====')
 
